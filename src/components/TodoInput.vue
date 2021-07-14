4e89116b25d662b61672ee2c852ad8d74a8409ab
iron0712 <office.div1201@gmail.com>
2021-07-14 18:33:11 +0900

    Add: TodoInput 컴포넌트 코드 정리 및 UI 스타일링

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,7 +1,9 @@
 <template>
-  <form action="">
-    <input type="text" v-model="newTodoItem" v-on:keyup:enter="addTodo" />
-    <button v-on:click="addTodo">추가</button>
+  <form class="inputBox shadow">
+    <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo" />
+    <span class="addContainer" v-on:click="addTodo">
+      <i class="fas fa-plus addBtn"></i>
+    </span>
   </form>
 </template>
 
@@ -16,6 +18,9 @@ export default {
     addTodo: function (e) {
       e.preventDefault();
       localStorage.setItem(this.newTodoItem, this.newTodoItem);
+      this.clearInput();
+    },
+    clearInput: function () {
       this.newTodoItem = "";
     },
   },
@@ -23,4 +28,28 @@ export default {
 </script>
 
 <style scoped>
+input:focus {
+  outline: none;
+}
+.inputBox {
+  background: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+.inputBox input {
+  border-style: none;
+  font-size: 0.9rem;
+}
+.addContainer {
+  float: right;
+  background: linear-gradient(to right, #6478fb, #8763fb);
+  display: block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+}
+.addBtn {
+  color: white;
+  vertical-align: middle;
+}
 </style>

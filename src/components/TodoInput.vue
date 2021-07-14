13481eecf8b5e2362cf02b0f8f6010e239e4eee7
iron0712 <office.div1201@gmail.com>
2021-07-14 16:19:33 +0900

    Add: 초기 세팅 및 컴포넌트 생성 및 등록

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,10 @@
+<template>
+  <form action="">input</form>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,12 @@
+<template>
+  <ul>
+    list
+  </ul>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
